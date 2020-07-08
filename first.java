847f0a7fefa302009eb57673c66610e801133b47
Ahel2000 <66062504+Ahel2000@users.noreply.github.com>
2020-07-08 13:53:53 +0530

    Add files via upload

---
+++ first.java
@@ -0,0 +1,37 @@
+package com.ahel.reminiscence;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class first extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Intent = new Intent(first.this,les.class);
+                startActivity(Intent);
+
+            }
+        });
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Intent = new Intent(first.this,metex.class);
+                startActivity(Intent);
+
+            }
+        });
+    }
+}
